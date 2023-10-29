9d8ce69f6b0de7bf388d575e496afc4ea1047dfe
canxin <1969730106@qq.com>
2023-10-29 17:34:47 +0800

    hello_world

---
+++ basic/hello-world/src/bin/main.rs
@@ -0,0 +1,22 @@
+//! Hello World.
+//! In this package, we use defmt with RTT to println hello to print "hello world" to the console.
+//! I highly recomended you to try learning defmt(https://defmt.ferrous-systems.com/introduction) more.
+//! Here I only use the most simply fn from defmt for example.
+#![no_std]
+#![no_main]
+use defmt::{error, info, println, warn};
+use defmt_rtt as _;
+use panic_probe as _;
+
+use stm32h7xx_hal::prelude::*;
+
+use cortex_m_rt::entry;
+// Run the following fn, and you will get the output from the console.
+#[entry]
+fn main() -> ! {
+    info!("This is a info.");
+    warn!("This is a warn.");
+    error!("This is a error.");
+    println!("This is a println.");
+    loop {}
+}
